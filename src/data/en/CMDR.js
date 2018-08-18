2e4908b596282696e2013f1bab9ee9f8c7c3292a
Haroen Viaene <hello@haroen.me>
2018-08-18 15:51:32 +0200

    feat: CMDR*008

--- src/data/en/CMDR.js
+++ src/data/en/CMDR.js
@@ -1,10 +1,69 @@
-import React, { Fragment } from 'react';
-import { Red } from '../../components/colors';
+import React from 'react';
+import { Red, Green } from '../../components/colors';
+import { Countdown } from '../../components/Countdown';
+import { random } from '../../utils';
 
 export default {
   logs: 'Welcome to the commander position',
   help: 'default help text for commander',
   OPS: {
+    '008': {
+      // TODO: find out which level this should have
+      getLogs: () => [
+        'OMS ignition 1',
+        <>
+          OMS fuel level: <Green>{random(80, 100)}</Green>%
+        </>,
+      ],
+      ITEM: {
+        '301': {
+          logs: [
+            <>
+              OMS-1: <Green>on</Green>
+            </>,
+            <>
+              OMS-2: <Green>on</Green>
+            </>,
+            <>
+              OMS-3: <Green>on</Green>
+            </>,
+          ],
+        },
+        '474': {
+          logs: [
+            <>
+              starting OMS flight profile: <Green>high</Green>
+            </>,
+            <Countdown from={10}>
+              <Green>OMS ignition</Green>
+            </Countdown>,
+          ],
+          endsOPS: true,
+        },
+        '475': {
+          logs: [
+            <>
+              starting OMS flight profile: <Green>normal</Green>
+            </>,
+            <Countdown from={10}>
+              <Green>OMS ignition</Green>
+            </Countdown>,
+          ],
+          endsOPS: true,
+        },
+        '476': {
+          logs: [
+            <>
+              starting OMS flight profile: <Green>low</Green>
+            </>,
+            <Countdown from={10}>
+              <Green>OMS ignition</Green>
+            </Countdown>,
+          ],
+          endsOPS: true,
+        },
+      },
+    },
     '001': {
       help: 'items that work are 001 and 002',
       ITEM: {
@@ -27,9 +86,9 @@ export default {
       ITEM: {
         '001': {
           getLogs: () => (
-            <Fragment>
+            <>
               Hey there this is my <Red>Item</Red>
-            </Fragment>
+            </>
           ),
           endsOPS: true,
         },
--- src/data/nl/CMDR.js
+++ src/data/nl/CMDR.js
@@ -1,11 +1,70 @@
 // deze imports zijn enkel nodig voor commands met React
 import React from 'react';
-import { Red } from '../../components/colors';
+import { Red, Green } from '../../components/colors';
+import { Countdown } from '../../components/Countdown';
+import { random } from '../../utils';
 
 export default {
   logs: 'Welkom commandant',
   help: 'Je bent een commandant, voer een operatie in waar gevraagd',
   OPS: {
+    '008': {
+      // TODO: find out which level this should have
+      getLogs: () => [
+        'OMS ontsteking 1',
+        <>
+          OMS brandstofniveau: <Green>{random(80, 100)}</Green>%
+        </>,
+      ],
+      ITEM: {
+        '301': {
+          logs: [
+            <>
+              OMS-1: <Green>ingeschakeld</Green>
+            </>,
+            <>
+              OMS-2: <Green>ingeschakeld</Green>
+            </>,
+            <>
+              OMS-3: <Green>ingeschakeld</Green>
+            </>,
+          ],
+        },
+        '474': {
+          logs: [
+            <>
+              initialisering van vluchtprogramma: <Green>hoog</Green>
+            </>,
+            <Countdown from={10}>
+              <Green>OMS ontsteking</Green>
+            </Countdown>,
+          ],
+          endsOPS: true,
+        },
+        '475': {
+          logs: [
+            <>
+              initialisering van vluchtprogramma: <Green>normaal</Green>
+            </>,
+            <Countdown from={10}>
+              <Green>OMS ontsteking</Green>
+            </Countdown>,
+          ],
+          endsOPS: true,
+        },
+        '476': {
+          logs: [
+            <>
+              initialisering van vluchtprogramma: <Green>laag</Green>
+            </>,
+            <Countdown from={10}>
+              <Green>OMS ontsteking</Green>
+            </Countdown>,
+          ],
+          endsOPS: true,
+        },
+      },
+    },
     '001': {
       help: '001, 002 en 003 zijn mogelijk',
       ITEM: {
