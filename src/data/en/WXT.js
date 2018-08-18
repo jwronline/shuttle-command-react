c3c0e63ba52ac2aca773cd590a32566a0116153e
Haroen Viaene <hello@haroen.me>
2018-08-18 19:12:46 +0200

    feat(WXT): *130 (weather)

--- src/data/en/WXT.js
+++ src/data/en/WXT.js
@@ -1,9 +1,59 @@
+import { random, table } from '../../utils';
+
 export default {
-  name: 'Welkom WXT',
+  logs: 'Welcome WXT',
   OPS: {
-    '001': {
+    '130': {
+      logs: 'Weather report',
+      help: ['ITEM 320: landing weather', 'ITEM 624: emergency weather'],
       ITEM: {
-        '001': { endsOPS: true },
+        '320': {
+          getLogs: () =>
+            table([
+              {
+                location: 'Kennedy',
+                temperature: `${random(15, 30)}°C`,
+                precipitation: `${random(5, 50)}%`,
+                humidity: `${random(0, 50)}%`,
+                wind: `${random(0, 50)} km/h`,
+              },
+              {
+                location: 'Edwards',
+                temperature: `${random(20, 35)}°C`,
+                precipitation: `${random(10, 40)}%`,
+                humidity: `${random(10, 40)}%`,
+                wind: `${random(16, 50)} km/h`,
+              },
+              {
+                location: 'White Sands',
+                temperature: `${random(15, 45)}°C`,
+                precipitation: `${random(10, 90)}%`,
+                humidity: `${random(10, 100)}%`,
+                wind: `${random(16, 150)} km/h`,
+              },
+            ]),
+          endsOPS: true,
+        },
+        '624': {
+          getLogs: () =>
+            table([
+              {
+                location: 'Dakar',
+                temperature: `${random(15, 30)}°C`,
+                precipitation: `${random(5, 10)}%`,
+                humidity: `${random(10, 25)}%`,
+                wind: `${random(16, 150)} km/h`,
+              },
+              {
+                location: 'White Sands',
+                temperature: `${random(15, 45)}°C`,
+                precipitation: `${random(10, 90)}%`,
+                humidity: `${random(10, 100)}%`,
+                wind: `${random(16, 150)} km/h`,
+              },
+            ]),
+          endsOPS: true,
+        },
       },
     },
   },
