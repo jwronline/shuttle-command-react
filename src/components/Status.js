beb30909b4016330fd6a9b55ef7c63796febd0b6
Haroen Viaene <hello@haroen.me>
2018-08-19 15:05:37 +0200

    chore(Status): use CurrentDate

--- src/components/Status.js
+++ src/components/Status.js
@@ -1,36 +1,17 @@
 import React, { Component } from 'react';
 import data from '../data';
-import { isoDate } from '../utils';
+import { CurrentDate } from './';
 import './Status.css';
 
 const posToString = pos => (pos ? data[pos].name : 'NONE');
 
 export class Status extends Component {
-  state = {
-    date: '',
-  };
-
-  componentDidMount() {
-    this.intervalId = setInterval(
-      () =>
-        this.setState({
-          date: isoDate(new Date()),
-        }),
-      1000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.intervalId);
-  }
-
   render() {
     const { POS, OPS, language, onChangeLanguage } = this.props;
     const { DATE } = data.shared[language];
-    const { date } = this.state;
     return (
       <div className="Status">
-        POS: {posToString(POS)} | OPS: {OPS || 'NONE'} | {DATE}: {date}
+        POS: {posToString(POS)} | OPS: {OPS || 'NONE'} | {DATE}: <CurrentDate />
         <select onChange={e => onChangeLanguage(e.target.value)}>
           <option value="en">EN</option>
           <option value="nl">NL</option>
