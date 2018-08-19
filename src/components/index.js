12c682620fe49a287425d30d43c85b093f7bd814
Haroen Viaene <hello@haroen.me>
2018-08-19 15:05:21 +0200

    chore: export new comps

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 export { Blue, Green, Orange, Red } from './colors';
 export { Countdown } from './Countdown';
+export { CurrentDate } from './CurrentDate';
 export { Input } from './Input';
 export { Log } from './Log';
+export { Stagger } from './Stagger';
 export { Status } from './Status';
